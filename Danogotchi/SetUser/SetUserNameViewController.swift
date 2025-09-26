7da7ececc0694480745a35d6bda47da9039fab1e
Maekiee <pdwssf@gmail.com>
2025-09-26 22:55:26 +0900

    키보드 업다운

--- Danogotchi/SetUser/SetUserNameViewController.swift
+++ Danogotchi/SetUser/SetUserNameViewController.swift
@@ -7,7 +7,7 @@ import SnapKit
 final class SetUserNameViewController: BaseViewController {
     private let disposeBag = DisposeBag()
     private let viewModel = SetUsernameViewModel()
-
+    
     // MARK: - UI 선언
     private let usernameTextField = UnderlineTextField(placeholder: "닉네임")
     private let confirmButton = PrimaryFillButton(title: "확인")
@@ -17,12 +17,13 @@ final class SetUserNameViewController: BaseViewController {
         label.font = .systemFont(ofSize: 13, weight: .semibold)
         return label
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configHierarchy()
         configLayout()
         configView()
+        setupKeyboardObserver()
         
         bind()
     }
@@ -48,7 +49,7 @@ final class SetUserNameViewController: BaseViewController {
         }
         
         confirmButton.snp.makeConstraints { make in
-            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-20)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
             make.horizontalEdges.equalToSuperview().inset(24)
             make.height.equalTo(44)
         }
@@ -57,7 +58,32 @@ final class SetUserNameViewController: BaseViewController {
     override func configView() {
         
     }
-
+    
+    // 키보드 업다운
+    private func setupKeyboardObserver() {
+        // 버튼 업
+        NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+            .compactMap { $0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect }
+            .bind(with: self) { owner, keyboardFrame in
+                owner.confirmButton.snp.updateConstraints { make in
+                    make.bottom.equalTo(owner.view.safeAreaLayoutGuide).offset(-keyboardFrame.height + 20)
+                }
+                UIView.animate(withDuration: 0.3) {
+                    owner.view.layoutIfNeeded()
+                }
+            }.disposed(by: disposeBag)
+        
+        // 버튼 다운
+        NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+            .bind(with: self) { owner, _ in
+                owner.confirmButton.snp.updateConstraints { make in
+                    make.bottom.equalTo(owner.view.safeAreaLayoutGuide).offset(-20)
+                }
+                UIView.animate(withDuration: 0.3) {
+                    owner.view.layoutIfNeeded()
+                }
+            }.disposed(by: disposeBag)
+    }
 }
 
 
