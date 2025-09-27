a331130d27923da69dcf1aa84653d0a4c92e0e23
Maekiee <pdwssf@gmail.com>
2025-09-27 13:59:05 +0900

    메인화면 이동 로직 추가

---
+++ Danogotchi/SettingTab/SettingTabViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingTabViewController.swift
+//  Danogotchi
+//
+//  Created by 박도원 on 9/27/25.
+//
+
+import Foundation
---
+++ Danogotchi/VocabTab/VocabTabViewController.swift
@@ -0,0 +1,8 @@
+//
+//  VocabTabViewController.swift
+//  Danogotchi
+//
+//  Created by 박도원 on 9/27/25.
+//
+
+import Foundation
