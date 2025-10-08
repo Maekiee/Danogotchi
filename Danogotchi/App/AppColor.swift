8c3d90188f1559810def938d408bf3be9b04beb0
Maekiee <pdwssf@gmail.com>
2025-10-08 16:27:52 +0900

    컬러쳇 추가

---
+++ Danogotchi/App/AppColor.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+enum AppColor {
+    static let appBackgroundColor = UIColor(red: 0.949, green: 0.9529, blue: 0.9608, alpha: 1.0)
+    static let cardColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1.0)
+//    static let warmOrange = UIColor(red: 1.00, green: 0.66, blue: 0.47, alpha: 1.00)
+//    static let primaryOrange = UIColor(red: 1.00, green: 0.43, blue: 0.11, alpha: 1.00)
+//    static let lightGray = UIColor(red: 0.82, green: 0.82, blue: 0.82, alpha: 1.00)
+//    static let textPrimary = UIColor(red: 0.20, green: 0.25, blue: 0.33, alpha: 1.00)
+//    static let mediumGray = UIColor(red: 0.67, green: 0.67, blue: 0.68, alpha: 1.00)
+}
