f079a868a8f3322d16b7f27d7abed664d0b64325
Maekiee <pdwssf@gmail.com>
2025-10-10 02:28:38 +0900

    primaryColor 변경

--- Danogotchi/App/AppColor.swift
+++ Danogotchi/App/AppColor.swift
@@ -5,5 +5,8 @@ enum AppColor {
     static let cardColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1.0)
     static let textPrimaryColor =  UIColor(red: 0.13, green: 0.13, blue: 0.13, alpha: 1)
     static let textSecondaryColor =  UIColor(red: 0.36, green: 0.36, blue: 0.38, alpha: 1)
-    static let primaryColor = UIColor(red: 0.58, green: 0.72, blue: 0.60, alpha: 1.0)
+    static let primaryColor = UIColor(red: 0.17, green: 0.24, blue: 0.31, alpha: 0.95)
+    
+    
+//    UIColor(red: 0.36, green: 0.68, blue: 0.89, alpha: 0.9)
 }
