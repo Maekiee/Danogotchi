b75d2473fcec1a897fe254a7f25c421fd4f82478
Maekiee <pdwssf@gmail.com>
2025-10-16 02:00:13 +0900

    푸시 추가

--- Danogotchi/App/AppDelegate.swift
+++ Danogotchi/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import FirebaseCore
+import FirebaseMessaging
 import IQKeyboardManagerSwift
 
 @main
@@ -9,8 +10,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        
         FirebaseApp.configure()
+        
         IQKeyboardManager.shared.isEnabled = true
+        
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+
+        application.registerForRemoteNotifications()
+        
+        Messaging.messaging().delegate = self
+        
+        Messaging.messaging().token { token, error in
+          if let error = error {
+            print("Error fetching FCM registration token: \(error)")
+          } else if let token = token {
+            print("FCM registration token: \(token)")
+          }
+        }
+        
+        
+        
         
         return true
     }
@@ -27,3 +53,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+//    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//        deviceToken
+//    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    // 디바이스 토큰 정보가 변견ㅇ이 되면 알려줌
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+}
