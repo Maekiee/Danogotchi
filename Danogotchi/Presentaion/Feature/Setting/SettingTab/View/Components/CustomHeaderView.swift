0f6472fcbc8357133af9ed1b3d788ca710cc9c48
Maekiee <pdwssf@gmail.com>
2026-05-16 14:38:13 +0900

    CustomHeaderView 분리

---
+++ Danogotchi/Presentaion/Feature/Setting/SettingTab/View/Components/CustomHeaderView.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class CustomHeaderView: UICollectionReusableView {
+    static let reuseIdentifier = "CustomHeaderView"
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .medium)
+        label.textColor = .black
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(label)
+        label.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(4)
+            make.top.equalToSuperview().offset(8)
+            make.bottom.equalToSuperview().offset(-8)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with text: String) {
+        label.text = text
+    }
+}
