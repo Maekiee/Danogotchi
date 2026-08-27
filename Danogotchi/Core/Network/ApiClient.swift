55205ef0c4561d27a9845a1789cc3716ce89a5db
Dowon <pdwssf@gmail.com>
2026-08-27 18:56:59 +0900

    fix: refactoer network logic to URLSession

--- Danogotchi/Core/Network/ApiClient.swift
+++ Danogotchi/Core/Network/ApiClient.swift
@@ -1,5 +1,38 @@
 import Foundation
 
+
+
+enum APIConfig {
+    private static let domain: String = value("UnsplashBaseURL")
+    private static let apiKeys: [String] = value("UnsplashAPIKeys")
+    
+    private static func value<T>(_ key: String) -> T {
+        guard let value = Bundle.main.object(forInfoDictionaryKey: key) as? T else {
+            fatalError("Info.plist에 \(key) 키가 없거나 타입이 맞지 않습니다.")
+        }
+        return value
+    }
+    
+    static let baseURL: String = "https://\(domain)"
+    static var apiKey: String { apiKeys.randomElement()! }
+}
+
+enum APIHeader {
+    case apiKey
+    case applicationJSON
+    
+    var field: (name: String, value: String) {
+        switch self {
+        case .apiKey: return ("Authorization", "client_id \(APIConfig.apiKey)")
+        case .applicationJSON: return ("Content-Type", "application/json")
+        }
+    }
+    static func dict(_ headers: [APIHeader]) -> [String: String] {
+        Dictionary(uniqueKeysWithValues: headers.map { ($0.field.name, $0.field.value)})
+    }
+}
+
+
 protocol ApiClientProtocol: Sendable {
     func request<T: Decodable>(_ endpoint: Endpoint, type: T.Type) async throws -> T
 }
@@ -13,32 +46,17 @@ enum HTTPMethod: String, Sendable {
 }
 
 protocol Endpoint: Sendable {
-    var baseURL: URL { get }
     var path: String { get }
     var method: HTTPMethod { get }
     var headers: [String: String] { get }
     var body: Data? { get }
-    var requiresAuth: Bool { get }
     var queryItems: [URLQueryItem]? { get }
 }
 
 extension Endpoint {
-    var headers: [String: String] {
-        return [
-            "Content-Type":"application/json",
-        ]
-    }
-    
-    var body: Data? {
-        return nil
-    }
-    
-    // 기본값: 인증 필요 (실수 방지)
-    var requiresAuth: Bool {
-        return true
-    }
-    
-    var queryItems: [URLQueryItem]? { return nil }
+    var headers: [String:String] { APIHeader.dict([.apiKey, .applicationJSON]) }
+    var body: Data? { nil }
+    var queryItems: [URLQueryItem]? { nil }
 }
 
 final class ApiClient: ApiClientProtocol {
@@ -50,14 +68,5 @@ final class ApiClient: ApiClientProtocol {
     
     func request<T>(_ endpoint: any Endpoint, type: T.Type) async throws -> T where T : Decodable {
         <#code#>
-    }
-    
-    private func createURL(from endpoint: Endpoint) -> URL? {
-        guard var components = URLComponents(url: endpoint.baseURL, resolvingAgainstBaseURL: true) else {
-            return nil
-        }
-        components.path += endpoint.path
-        components.queryItems = endpoint.queryItems
-        return components.url
     }
 }
